c53c2eb87744634c97b785fb7a4509b7172cde4b
dammy995 <damianemerah1@gmail.com>
2022-09-10 17:48:34 -0700

    Low level language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in printf function
+ *
+ * Description: using the main function 
+ * this prigram prints the size of data type
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long l;
+	long long ll;
+	float f;
+
+	printf("Size of char: %ld byte(s)\n", sizeof(c));
+	printf("Size of int: %ld byte(s)\n", sizeof(i));
+	printf("Size of long: %ld byte(s)\n", sizeof(l));
+	printf("Size of long long: %ld byte(s)\n", sizeof(ll));
+	printf("Size of float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
