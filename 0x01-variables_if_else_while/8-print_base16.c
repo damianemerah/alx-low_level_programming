72d3485a168883a8b0eaa72a6ee4219b8251fadf
dammy995 <damianemerah1@gmail.com>
2022-09-11 10:02:07 -0700

    C conditionals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Description: The numbers are all single digits
+ *
+ * Return: Always(0) Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+
+}
