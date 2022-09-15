9c1763ebb8204a087bd3a03acab0ac906dc3e3e5
dammy995 <damianemerah1@gmail.com>
2022-09-15 15:39:33 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 - 9
+ *
+ * Return: noting
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= '9')
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+}
