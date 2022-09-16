28b425ded2315accdb999dfedd918af3c81d6957
dammy995 <damianemerah1@gmail.com>
2022-09-15 17:26:17 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** print_line - Print line
+ *
+ * @n: number of lines to print
+ * Description: Print line for number of times
+ */
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
