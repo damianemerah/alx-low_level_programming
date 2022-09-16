35d045947884c9b90ac5700633e86999e313966e
dammy995 <damianemerah1@gmail.com>
2022-09-15 17:03:24 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,19 @@
  */
 void more_numbers(void)
 {
-	int i = '0';
+	int i = 0;
 	int j;
 
-	while (i <= '5')
+	while (i <= 5)
 	{
-		j = '0';
-		while (j <= '14')
+		j = 0;
+		while (j <= 14)
 		{
-			_putchar(j);
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
 			j++;
 		}
 		i++;
