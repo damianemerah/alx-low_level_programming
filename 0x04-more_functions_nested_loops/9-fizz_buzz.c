870c727a27d9ec00ee7ae0abd6b3dd513f7d84d2
dammy995 <damianemerah1@gmail.com>
2022-09-15 21:19:58 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,17 +15,17 @@ void fizz_buzz(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("%s ", f);
+			printf("%s ", fb);
 		}
 		else if (i % 5 == 0)
 		{
 			printf("%s ", b);
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 3 == 0)
 		{
-			printf("%s ", fb);
+			printf("%s ", f);
 		}
 		else
 		{
