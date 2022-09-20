4059dd174244410b0624d476048e995bf2acdef0
dammy995 <damianemerah1@gmail.com>
2022-09-20 11:21:26 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - Print str in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int len = strlen(*s);
+
+	while (len--)
+	{
+		_putchar(*(s + len));
+	}
+	_putchar('\n');
+}
