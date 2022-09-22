07048c564a7258a5de6de3d9e332666f454dec21
dammy995 <damianemerah1@gmail.com>
2022-09-22 14:15:58 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,9 +8,10 @@
 char *string_toupper(char *str)
 {
 	int index = 0;
+
 	while (str[index])
 	{
-		if (str[index >= 'a' && str[index] <= 'z')
+		if (str[index] >= 'a' && str[index] <= 'z')
 			str[index] -= 32;
 		index++;
 	}
