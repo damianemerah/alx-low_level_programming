ffa4361987951244fc1d63618f40560828658520
dammy995 <damianemerah1@gmail.com>
2022-09-27 14:15:53 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,13 +5,14 @@
  *  @s: String
  *  @accept: String to compare with
  *  Return: Returns the number of bytes in the initial
- *  segment of s which consist only of bytes from accept 
+ *  segment of s which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; s[i]; i++){
+	for (i = 0; s[i]; i++)
+	{
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
