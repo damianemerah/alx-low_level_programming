3bdc858f73ffeb19856a01bc7cab88e834751d73
dammy995 <damianemerah1@gmail.com>
2022-09-28 16:26:27 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,12 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if (i == '\0')
+	if ((s + i) == '\0')
+	{
+		_putchar('\n');
 		return;
+	}
 	_putchar(s + i);
 	i++;
-	_puts_recursion(*s);
+	_puts_recursion(s);
 }
