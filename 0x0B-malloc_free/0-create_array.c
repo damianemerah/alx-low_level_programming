65fd08bf9a30866457a1d58333bac34179e4251f
dammy995 <damianemerah1@gmail.com>
2022-10-04 15:05:23 -0700

    Task 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	array = malloc(sizeof(char) * size);
+	array =  (char *)malloc(sizeof(c) * size);
 
 	if (array == 0)
 		return (NULL);
