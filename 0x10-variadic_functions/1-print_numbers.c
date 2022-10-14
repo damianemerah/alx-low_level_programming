7faca5e273ea298c69bfa9b3be729da45f912bf8
dammy995 <damianemerah1@gmail.com>
2022-10-13 17:59:54 -0700

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Print all numbers seperated by seperator
+ * @separator: Separator
+ * @n: Number of numbers to print
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+{
+	va_list nums;
+	unsigned int i;
+
+	va_start(nums, n);
+
+	if (separator == NULL)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nums, int));
+		if (i != (n - 1))
+			printf(separator);
+	}
+	printf('\n');
+
+	va_end(nums);
+}
