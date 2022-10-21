a4ab810a6b0d856a97b3604242959b29d010d3d2
dammy995 <damianemerah1@gmail.com>
2022-10-21 12:39:48 -0700

    Add list to node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -27,7 +27,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->next = NULL;
 	cur_node = *head;
 
-	if (cur_node->next == NULL)
+	if (cur_node == NULL)
 		*head = new_node;
 	else
 	{
