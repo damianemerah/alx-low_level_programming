ccce5143469f4dc8fe009ca023a17e37d0db3cb9
dammy995 <damianemerah1@gmail.com>
2022-10-21 12:50:48 -0700

    Free node list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - A function that frees a list_t list
+ * @head: Head of node
+ */
+
+void free_list(list_t *head)
+{
+	list_t *cur_node;
+
+	cur_node = *head;
+
+	while (cur_node != NULL)
+	{
+		free(cur_node->str);
+		free(cur_node);
+		cur_node = cur_node->next;
+	}
+}
