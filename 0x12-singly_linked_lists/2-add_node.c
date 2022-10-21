01287e29d9bf1aaabd5523c8f2b28eab0e20e906
dammy995 <damianemerah1@gmail.com>
2022-10-21 08:08:29 -0700

    Add item to beginning of nodelist

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,4 +11,19 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node;
 
+	new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (!new_node)
+		return (NULL);
+	new_node->str = strdup(str);
+	
+	for (n = 0; str[n]; n++);
+
+	new_node->len = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+}
