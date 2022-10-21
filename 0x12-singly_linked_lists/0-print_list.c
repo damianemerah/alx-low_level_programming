95b8ace96c0e3d45be128d079f0cde78a504b9f6
dammy995 <damianemerah1@gmail.com>
2022-10-21 07:01:21 -0700

    Print node list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "list.h"
+
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: the head of the list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		printf("[%u] %s\n", h->len, h->str);
+
+		node++;
+
+		h = h->next;
+	}
+	return (nodes);
+}
