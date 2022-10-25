5c5e5432a10e9a57878d5297bb2b15328af30e84
dammy995 <damianemerah1@gmail.com>
2022-10-25 06:11:20 -0700

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements
+ * 		linked to listint_t list
+ *
+ * @n: A pointer to the head of the listint_t list
+ * Return: the number of element in listint_t list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
