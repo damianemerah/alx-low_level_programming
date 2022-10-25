62af01989f4d898be383fbf477636f3bd5327683
dammy995 <damianemerah1@gmail.com>
2022-10-25 15:22:32 -0700

    Task 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (temp == NULL || temp->next == NULL)
 			return (NULL);
-		temp->next = new;
+		temp = temp->next;
 	}
 	new->next = temp->next;
 	temp->next = new;
