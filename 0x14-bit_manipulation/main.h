498914002d2bf86bb6b675c1ac653cae8aa0e2d5
dammy995 <damianemerah1@gmail.com>
2022-10-27 11:42:51 -0700

    Task 0 and 1

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+void print_binary(unsigned long int n);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+
+#endif
