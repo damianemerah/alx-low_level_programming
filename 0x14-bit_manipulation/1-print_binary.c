42046f5ef73aaccd1e2fd2671938b6170d473a29
dammy995 <damianemerah1@gmail.com>
2022-10-27 12:47:07 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,12 +6,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n >> 0)
-	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
-	}
-	else
-		_putchar('0')
+	if (n > 0)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
