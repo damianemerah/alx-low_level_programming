e625b265864bc4e327c01a7c59aba1496e886992
dammy995 <damianemerah1@gmail.com>
2022-10-27 12:19:04 -0700

    Task 0 and 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,24 +10,26 @@
  *         there is one or more chars in the string b that is not 0 or 1
  *         b is NULL
  */
-
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int ui;
 	int len, base_two;
-	unsigned int dec_val;
 
 	if (!b)
 		return (0);
-	dec_val = 0;
-	for (len = 0; b[len]; len++)
+	ui = 0;
+	for (len = 0; b[len] != '\0'; len++)
 		;
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
+		}
 		if (b[len] & 1)
-			dec_val += base_two;
+		{
+			ui += base_two;
+		}
 	}
-
-	return (dec_val);
+	return (ui);
 }
