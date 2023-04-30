7f77e57de81f83aacd0411ab176e926faa180527
Damian <damianemerah1@gmail.com>
2023-04-30 08:39:50 -0700

    advanced task

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,7 +6,8 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int min(int, int);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
